9e7fa4fd00d736b2677bf13d3ee585d0de5d3b4f
MineTech0 <niilo.kurki@hotmail.fi>
2020-08-06 16:36:36 +0300

    fixed register route

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('/home');
 });
-
+Route::get('/register', function () {
+    return abort(404);
+});
 Auth::routes();
 
 //Guarded routes
