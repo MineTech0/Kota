65fa9b2baf4933d738244872470543c3c74f72df
Niilo Kurki <niilo.kurki@hotmail.fi>
2022-01-22 00:19:11 +0200

    added equipment migration

---
+++ database/migrations/2020_08_10_201033_create_equipment_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEquipmentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('equipment', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('weight');
+            $table->string('form');
+            $table->string('location');
+            $table->integer('quantity');
+            $table->integer('loan_time');
+            $table->text('info');
+            $table->string('serial');
+            $table->string('picture');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('equipment');
+    }
+}
