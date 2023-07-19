1a2233698f35a2daeb4bac88d1df7140923fac82
Niilo Kurki <niilo.kurki@hotmail.fi>
2023-07-19 21:36:42 +0300

    deplo scripts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,6 @@
     <!-- Scripts -->
     <script src="{{ mix('js/app.js') }}"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.21/datatables.min.js"></script>
-    <script src="{{ asset('js/fileinput.js') }}"></script>
     <script src="{{ asset('js/main.js') }}"></script>
     @yield('script')
     @stack('scripts')
