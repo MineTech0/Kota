1d42bea1e5b7a64646eb12435f4aa4cf43fe1f62
Niilo Kurki <niilo.kurki@hotmail.fi>
2023-10-22 23:58:34 +0300

    added perhepartio age

--- resources/js/components/AgeGroupTag.vue
+++ resources/js/components/AgeGroupTag.vue
@@ -2,14 +2,14 @@
 import { ParentAgeGroup } from "@/types";
 import { NTag } from "naive-ui";
 import { computed } from "vue";
-import { ParentAgeGroupColor } from "@/colors";
+import { ParentAgeGroupColor, ScoutColor } from "@/colors";
 import { getTextColor } from "@/utils/commontUtils";
 
 const props = defineProps<{
     parentAgeGroup: ParentAgeGroup;
 }>();
 
-const color = computed(() => ParentAgeGroupColor[props.parentAgeGroup]);
+const color = computed(() => ParentAgeGroupColor[props.parentAgeGroup] ?? ScoutColor.PRIMARY);
 const textColor = computed(() => getTextColor(color.value));
 </script>
 <template>
